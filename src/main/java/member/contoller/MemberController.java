93f910ab3db854731e06fb413528300440fff813
Jo <Jo@Joshinyeong>
2019-08-29 16:35:29 +0900

    MemberController

--- src/main/java/member/contoller/MemberController.java
+++ src/main/java/member/contoller/MemberController.java
@@ -1,5 +1,16 @@
 package member.contoller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import member.domain.MemberVO;
 import member.service.MemberService;
 
 public class MemberController {
@@ -8,6 +19,25 @@ public class MemberController {
 
 	public void setMemberService(MemberService memberService) {
 		this.memberService = memberService;
+	}
+	@RequestMapping(value = "/member/main", method = RequestMethod.GET)
+	public String main(Model model) {
+		return "/member/main";
+	}
+	@RequestMapping(value = "/member/main", method = RequestMethod.POST)
+	public String login(Model model, HttpServletRequest req, HttpSession session)throws Exception{
+		String email = req.getParameter("email");
+		String pass = req.getParameter("password");
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("email", email);
+		map.put("password", pass);
+		MemberVO memberVO = memberService.memberLogin(map);
+		if (memberVO == null) {
+			return "redirect:/member/main";
+		} else {
+			session.setAttribute("member", memberVO);
+			return "redirect:/board/list";
+		}
 	}
 
 }
